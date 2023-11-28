b8c0a984faaebb8a953036676bd653174a086e83
dom <domcaporuscio@gmail.com>
2023-11-27 22:55:33 -0500

    new website style, and added privacy policy

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        game_background: "#1b2038",
+        arrow_blue: "#acaaff",
+        arrow_pink: "#ff8a9a",
+        imessage_blue: "#087fff",
+        imessage_gray: "#e9e9eb",
+        imessage_header_background: "#f5f5f5",
+      },
+    },
+  },
+  plugins: [],
+};
